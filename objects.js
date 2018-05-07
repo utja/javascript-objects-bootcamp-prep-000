c1e593cc23e529b93684c1ec217370964064a6df
ahn.jon.s@gmail.com Ahn <ahn.jon.s@gmail.com>
2018-05-07 23:55:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName]
+=======
+  delete playlist.artistName
+>>>>>>> e8ef28a09ce9fdd6a85c0d9f750c4ad46384c52f
   return playlist
 }
